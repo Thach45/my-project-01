78a73af907c02d5191516b730f74eaff819c92e7
Thach45 <nguyenhoangthach2005@gmail.com>
2024-07-09 15:09:04 +0700

    fix bug

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.mongoURL);
+        await mongoose.connect("mongodb+srv://nguyenhoangthach:Thach18012005@cluster0.hgcnpf3.mongodb.net/project-01");
         console.log("Connect ok");
     } catch (error) {
         console.log("Connect fail");
