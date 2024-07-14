8ce80848c80c74007dad2c15c193afd2993f9b5c
Thach45 <nguyenhoangthach2005@gmail.com>
2024-07-14 19:39:05 +0700

    init middlewares

--- routers/admin/product.routers.js
+++ routers/admin/product.routers.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer')
-const storage = require("../../helpers/storageMulti");
-// cloudinary
 const upload = multer()
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
-cloudinary.config({
-    cloud_name: process.env.cloudName,
-    api_key: process.env.cloudKey,
-    api_secret: process.env.cloudSecrit // Click 'View Credentials' below to copy your API secret
-});
-
-// end cloudinary
+const storage = require("../../helpers/storageMulti");
+const uploadOnline = require("../../middlewares/admin/uploadOnline");
 
 
 //Cấu hình controller
@@ -23,73 +14,9 @@ router.patch('/change-status/:status/:id', product.changeStatus);
 router.patch('/change-multi', product.changeMulti);
 router.delete('/delete/:id', product.delete);
 router.get('/create', product.create);
-router.post('/create', upload.single('thumbnail'), function (req, res, next) {
-    if(req.file)
-    {
-
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                    (error, result) => {
-                        if (result) {
-                            resolve(result);
-                        } else {
-                            reject(error);
-                        }
-                    }
-                );
-    
-                streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-    
-        async function upload(req) {
-            let result = await streamUpload(req);
-            console.log(result);
-            req.body[req.file.fieldname] = result.url
-            next();
-        }
-        upload(req);
-    }
-    else
-    {
-        next();
-    }
-}, product.createP);
+router.post('/create', upload.single('thumbnail'),uploadOnline.upload, product.createP);
 router.get('/edit/:id', product.edit);
-router.patch('/edit/:id', upload.single('thumbnail'),function (req, res, next) {
-    if(req.file)
-    {
-
-        let streamUpload = (req) => {
-            return new Promise((resolve, reject) => {
-                let stream = cloudinary.uploader.upload_stream(
-                    (error, result) => {
-                        if (result) {
-                            resolve(result);
-                        } else {
-                            reject(error);
-                        }
-                    }
-                );
-    
-                streamifier.createReadStream(req.file.buffer).pipe(stream);
-            });
-        };
-    
-        async function upload(req) {
-            let result = await streamUpload(req);
-            console.log(result);
-            req.body[req.file.fieldname] = result.url
-            next();
-        }
-        upload(req);
-    }
-    else
-    {
-        next();
-    }
-} ,product.editP);
+router.patch('/edit/:id', upload.single('thumbnail'),uploadOnline.upload, product.editP);
 
 
 
